d4e95fbe3c4d334b63ef211d9f56f6b257228666
Adel <adelchik022005@gmail.com>
2025-11-05 17:40:35 +0300

    new instructions2

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -512,8 +512,11 @@ async def menu_instruction_detail(callback: CallbackQuery, bot: Bot):
     photo_file_ids = {
         "android": "AgACAgIAAxkBAAIB... (",
         "ios": "AgACAgIAAxkBAAICq2kLROhKfgWv-anm5RLrPQ6moeDeAAIkC2sbS6VJSF1oKppWVA0qAQADAgADeQADNgQ",
-        "windows": "AgACAgIAAxkBAAIB... (",
-        "macos": "AgACAgIAAxkBAAIB... ("
+        "windows": [
+            "AgACAgIAAxkBAAICIWkLWnL0GRAzIcCd4V-ECnF3-3O0AAJdD2sbVAJYSCHqt6zI0c4cAQADAgADeAADNgQ",
+            "AgACAgIAAxkBAAICH2kLWmRvaP2PDReDiJAJA73uclRaAAJcD2sbVAJYSBnNd5oZOSROAQADAgADeAADNgQ"
+        ],
+        "macos": "AgACAgIAAxkBAAICHWkLWlrP9u6ZIKZwz5mjAAGcTPg5hQACVw9rG1QCWEiJXH2RydyIewEAAwIAA3gAAzYE"
     }
 
     platform = callback.data.split(":")[1]
