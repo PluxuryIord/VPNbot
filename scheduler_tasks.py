d66ebbe26af2b6a08e9c047cdd156b6f9a7d49d0
Adel <adelchik022005@gmail.com>
2025-11-14 02:52:14 +0300

    sheduler time

--- scheduler_tasks.py
+++ scheduler_tasks.py
@@ -96,4 +96,4 @@ async def check_expirations(bot: Bot):
         except Exception as e:
             log.error(f"Error in expiration checker task: {e}")
 
-        await asyncio.sleep(60)  # Проверка каждые 10 минут
+        await asyncio.sleep(600)  # Проверка каждые 10 минут
